8a2624aa5421065f7224cadef2144a5400b54e95
magentacoconut41 <aurelius.wong422@gmail.com>
2025-02-26 10:47:33 +0800

    cgfhjkjlk

--- src/main/java/AnagramSolver.java
+++ src/main/java/AnagramSolver.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.*;
+import java.util.*;
 
 public class AnagramSolver {
 
@@ -11,8 +11,40 @@ public class AnagramSolver {
      * @param filename
      * @return
      */
-    public static HashMap<String, ArrayList<String>> anagrams(String filename) {
-        return null;
+    public static HashMap<String, ArrayList<String>> anagrams(String filename) throws IOException {
+        HashMap<String, ArrayList<String>> output = new HashMap<String, ArrayList<String>>();
+        List<String> wordList = new ArrayList<String>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("words_alpha.txt"));
+        String line = bufferedReader.readLine();
+        wordList.add(line);
+        while(line != null){
+            line = bufferedReader.readLine();
+            wordList.add(line);
+        }
+        String[] wordArray = wordList.toArray(new String[0]);
+        bufferedReader.close();
+        for(int i = 0; i < wordArray.length; i++){
+            if(output.get(sortString(wordArray[i])) != null){
+                ArrayList<String> anagramWords = new ArrayList<>();
+                anagramWords.add(wordArray[i]);
+                output.put(sortString(wordArray[i]), anagramWords);
+            }else {
+                if (!output.get(sortString(wordArray[i])).contains(wordArray[i])) {
+                    ArrayList<String> temp = output.get(sortString(wordArray[i]));
+                    temp.add(wordArray[i]);
+                    output.remove(sortString(wordArray[i]));
+                    output.put(sortString(wordArray[i]), temp);
+                }
+            }
+        }
+        return output;
+    }
+
+    public static String sortString(String input){
+        char[] tempArray = input.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+
     }
 
     /**
@@ -22,7 +54,16 @@ public class AnagramSolver {
      * @return
      */
     public static ArrayList<String> mostFrequentAnagram(HashMap<String, ArrayList<String>> anagrams) {
-        return null;
+        ArrayList<ArrayList<String>> compare = new ArrayList<>(anagrams.values());
+        ArrayList<String> largest = compare.getFirst();
+        for(int i = 0; i < compare.size(); i++){
+            ArrayList<String> currentList = compare.get(i);
+            if(largest.size() < currentList.size()){
+                largest = currentList;
+            }
+        }
+        return largest;
+
     }
 
     /**
@@ -31,7 +72,12 @@ public class AnagramSolver {
      * @param anagrams
      */
     public static void printKeyValuePairs(HashMap<String, ArrayList<String>> anagrams) {
-
+        for(Map.Entry<String, ArrayList<String>> entry : anagrams.entrySet()){
+            System.out.println("Key: " + entry.getKey());
+            for(String s: entry.getValue()){
+                System.out.println("Anagram values: " + s);
+            }
+        }
     }
 
 }
